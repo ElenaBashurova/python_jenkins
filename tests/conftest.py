9d54bc81eb7692397cb1d99cfbebd572734dbc73
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-01-04 18:52:21 +0300

    jenkins

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selene import browser
+from utils import attach
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+
+@pytest.fixture(scope='function')
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 2.0
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    # driver = webdriver.Remote(
+    #     command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+    #     options=options)
+    #
+    # browser.config.driver = driver
+
+    yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
---
+++ tests/test_demoqa.py
@@ -0,0 +1,36 @@
+import allure
+from selene import browser, have, by
+import os
+from selene.support.shared.jquery_style import s
+
+
+def test_steps(browser_management):
+    with allure.step('Открываем страницу и проверяем текст'):
+        browser.open("/automation-practice-form")
+        s('.practice-form-wrapper').should(have.text('Student Registration Form'))
+    with allure.step('Вводим данные пользователя'):
+        s('#firstName').type('Romanov')
+        s('#lastName').type('Ivan')
+        s('#userEmail').type('romanov.i@mail.com')
+        s('#gender-radio-1').double_click()
+        s('#userNumber').type('9087658909')
+        s('#dateOfBirthInput').click()
+        s('.react-datepicker__month-select').click().element(by.text('February')).click()
+        s('.react-datepicker__year-select').click().element(by.text('2002')).click()
+        s('.react-datepicker__day--006').click()
+        s('#subjectsInput').send_keys('Maths').press_enter()
+        s('[for=hobbies-checkbox-2]').click()
+        s('#uploadPicture').send_keys(os.path.abspath('picture/images.jpeg'))
+        s('#currentAddress').type('city Moscow, street Lenina')
+        s('#react-select-3-input').type('Haryana').press_enter()
+        s('#react-select-4-input').type('Karnal').press_enter()
+        s('#submit').click()
+
+    with allure.step('Проверка данных'):
+        s('.modal-header').should(have.text('Thanks for submitting the form'))
+        browser.all('tbody tr td:last-child').should(have.exact_texts(
+        'Romanov Ivan', 'romanov.i@mail.com', 'Male', '9087658909', '06 February,2002', 'Maths',
+        'Reading', 'images.jpeg', 'city Moscow, street Lenina', 'Haryana Karnal'))
+
+    with allure.step('Закрыть окно'):
+        s('.modal-footer').click()
