7ace0f01d9e72fde16ecc0dbc98d60dd82b5c84d
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-01-04 19:44:57 +0300

    jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,33 +5,32 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-
 @pytest.fixture(scope='function')
 def setup_browser(request):
-        browser.config.window_width = 1920
-        browser.config.window_height = 1080
-        browser.config.base_url = 'https://demoqa.com'
-        options = Options()
-        selenoid_capabilities = {
-            "browserName": "chrome",
-            "browserVersion": "100.0",
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": True
-            }
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com'
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
         }
-        options.capabilities.update(selenoid_capabilities)
-        driver = webdriver.Remote(
-            command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-            options=options
-        )
+    }
+    # options.capabilities.update(selenoid_capabilities)
+    # driver = webdriver.Remote(
+    #     command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+    #     options=options
+    # )
+    #
+    # browser.config.driver = driver
 
-        browser.config.driver = driver
+    yield
 
-        yield
-
-        attach.add_html(browser)
-        attach.add_screenshot(browser)
-        attach.add_logs(browser)
-        attach.add_video(browser)
-        browser.quit()
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
